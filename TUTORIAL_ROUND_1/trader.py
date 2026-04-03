71ee2d0d567e4d511f8f502a3530238294b7f4cc
Nosmas Quentin <qnosmas@gmail.com>
2026-04-03 15:59:09 +0200

    stratégie_émmeraudes

--- TUTORIAL_ROUND_1/trader.py
+++ TUTORIAL_ROUND_1/trader.py
@@ -4,4 +4,12 @@ from typing import List, Dict
 class Trader:
 
     def run(self, state: TradingState) -> Dict[str, List[Order]]:
+
+        # On initialise le dictionnaire d'ordres 
+        orders: Dict[str, List[order]] = {}
+        
+
+
+
+
         
